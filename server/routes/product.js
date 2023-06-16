e8b0177fb17b965e73f4b1aa585582f70fccd2ce
Krushn219 <krushn.219@gmail.com>
2023-06-16 12:03:52 +0530

    changes in product route

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,45 +1,72 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 const {
   filtersProductsall,
   filtersProducts,
   filtersProductsyear,
   filtersProductsweek,
 } = require("../controllers/productControllers");
+// const productControllers = require("../controllers/productControllers");
+// const { verifyToken, verifyTokenAndAdmin } = require("../jwt/jsonwebtoken");
+// const path = require("path");
+// const fs = require("fs");
+// // const upload = require('../services/multer');
+// const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+// const uploads = "../";
+
+// //cloudinary
+
+// const multer = require("multer");
+// const cloudinary = require("cloudinary").v2;
+
+// if (!fs.existsSync("./uploads")) {
+//   fs.mkdirSync("./uploads");
+// }
+
+// var storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "./uploads");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage: storage }).single("image");
+
+// // const upload = multer({ storage: storage }).single("image");
+// const uploadMultiple = multer({ storage: storage }).array("image", 6);
+
+const express = require("express");
+const router = express.Router();
 const productControllers = require("../controllers/productControllers");
-const { verifyToken, verifyTokenAndAdmin } = require("../jwt/jsonwebtoken");
-const path = require("path");
-const fs = require("fs");
-// const upload = require('../services/multer');
-const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
-const uploads = "../";
-
-//cloudinary
-
+const { isAuthenticatedUser } = require("../middleware/auth");
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 
-if (!fs.existsSync("./uploads")) {
-  fs.mkdirSync("./uploads");
-}
-
-var storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
+//cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
-const upload = multer({ storage: storage }).single("image");
+// Multer storage configuration
+const storage = multer.diskStorage({});
 
-// const upload = multer({ storage: storage }).single("image");
-const uploadMultiple = multer({ storage: storage }).array("image", 6);
+// Multer upload instance
+const upload = multer({ storage });
 
-router.post("/image", uploadMultiple, productControllers.image);
+router.post("/image", upload.array("image", 6), productControllers.image);
+router.post(
+  "/addProduct",
+  upload.array("image", 6),
+  productControllers.addProduct
+);
 
-router.post("/addProduct", uploadMultiple, productControllers.addProduct);
+// router.post("/image", uploadMultiple, productControllers.image);
+// router.post("/addProduct", uploadMultiple, productControllers.addProduct);
+
 // router.post('/multipleImages',uploadMultiple,productControllers.addProduct)
 
 //Get All products
@@ -49,19 +76,25 @@ router.get("/all", productControllers.productList);
 router.delete("/", productControllers.deleteProduct);
 
 //Update Product
-router.put("/update/:id", uploadMultiple, productControllers.updateProduct);
+// router.put("/update/:id", uploadMultiple, productControllers.updateProduct);
+router.put(
+  "/update/:id",
+  upload.array("image", 6),
+  productControllers.updateProduct
+);
 
 //Get single Product
 router.get("/single/:id", productControllers.getSingleProduct);
 
 //Create Review for Product
 router.put(
-  "/review", isAuthenticatedUser,
+  "/review",
+  isAuthenticatedUser,
   productControllers.createProductReview
 );
 
 //Update Product
-router.put("/:id", upload, productControllers.updateProduct);
+router.put("/:id", upload.single("image"), productControllers.updateProduct);
 
 //Get All Review of a product
 router.get("/productReview", productControllers.getProductReviews);
@@ -81,19 +114,31 @@ router.get(
 router.get("/category/:category", productControllers.getcategoryProduct);
 
 // product by subCategory
-router.get("/subCategory/:subcategories", productControllers.getsubCategoryProduct);
+router.get(
+  "/subCategory/:subcategories",
+  productControllers.getsubCategoryProduct
+);
 
 // group by product
 router.get("/group/:group", productControllers.getgroupProduct);
 
-//product by main category & category 
-router.get("/maincategory/:maincategory/category/:category", productControllers.getfiltercategoryP);
+//product by main category & category
+router.get(
+  "/maincategory/:maincategory/category/:category",
+  productControllers.getfiltercategoryP
+);
 
 // product by maincategory & category & subCategory
-router.get("/maincategory/:maincategory/category/:category/subcategories/:subcategories", productControllers.getfilterSubCategoryP);
+router.get(
+  "/maincategory/:maincategory/category/:category/subcategories/:subcategories",
+  productControllers.getfilterSubCategoryP
+);
 
 // product  by maincategory & category & subCategory & group
-router.get("/maincategory/:maincategory/category/:category/subcategories/:subcategories/group/:group", productControllers.getfiltergroupp);
+router.get(
+  "/maincategory/:maincategory/category/:category/subcategories/:subcategories/group/:group",
+  productControllers.getfiltergroupp
+);
 
 // all products
 router.route("/data/all").get(filtersProductsall);
