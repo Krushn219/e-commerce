5ee926a3c5cae2cfd28808e571f85ab77b2aa3d7
Krushn219 <krushn.219@gmail.com>
2023-06-16 17:53:16 +0530

    changes in axios

--- client/src/Utils/APIs/axios.js
+++ client/src/Utils/APIs/axios.js
@@ -1,86 +1,88 @@
-import axios from 'axios'
-import { Cookies } from 'react-cookie';
-const cookie = new Cookies()
+import axios from "axios";
+import { Cookies } from "react-cookie";
+const cookie = new Cookies();
+const REACT_APP_SERVER_URL = "https://e-commerce-api-omega.vercel.app";
 
-const Timeout = 60 * 1000
+const Timeout = 60 * 1000;
 
-const api = (url = '', method = 'POST', data = {}) => {
+const api = (url = "", method = "POST", data = {}) => {
   const options = {
     method,
     headers: {
-      'content-type': 'application/json',
+      "content-type": "application/json",
     },
     timeout: Timeout,
     data,
-    url: `${process.env.REACT_APP_SERVER_URL}/${url}`,
-  }
-  return axios(options)
-}
+    url: `${REACT_APP_SERVER_URL}/${url}`,
+  };
+  return axios(options);
+};
 
-export const apiVerify = (url = '', method = 'POST', data = {}) => {
-  const options = {
-    method,
-    headers: {
-      // 'content-type': 'application/json',
-    },
-    timeout: Timeout,
-    data,
-    url: `${process.env.REACT_APP_SERVER_URL}/${url}`,
-  }
-  return axios(options)
-}
-
-export const apiWithToken = (url = '', method = 'POST', data = {}) => {
-  const options = {
-    method,
-    headers: {
-      'content-type': 'application/json',
-      'x-access-token' : getToken()
-    },
-    timeout: Timeout,
-    data,
-    url: `${process.env.REACT_APP_SERVER_URL}/${url}`,
-  }
-  return axios(options)
-}
-
-export const apiUpload = (url = '', method = 'POST', data = {}) => {
+export const apiVerify = (url = "", method = "POST", data = {}) => {
   const options = {
     method,
     headers: {
       // 'content-type': 'application/json',
-      'x-access-token': getToken()
     },
     timeout: Timeout,
     data,
-    url: `${process.env.REACT_APP_SERVER_URL}/${url}`,
-  }
-  return axios(options)
-}
+    url: `${REACT_APP_SERVER_URL}/${url}`,
+  };
+  return axios(options);
+};
 
-export const apiFormdata = (url = '', method = 'POST', data = {}) => {
+export const apiWithToken = (url = "", method = "POST", data = {}) => {
   const options = {
     method,
     headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'x-access-token' : getToken()
+      "content-type": "application/json",
+      "x-access-token": getToken(),
     },
     timeout: Timeout,
     data,
-    url: `${process.env.REACT_APP_SERVER_URL}/${url}`,
-  }
-  return axios(options)
-}
+    url: `${REACT_APP_SERVER_URL}/${url}`,
+  };
+  return axios(options);
+};
+
+export const apiUpload = (url = "", method = "POST", data = {}) => {
+  const options = {
+    method,
+    headers: {
+      // 'content-type': 'application/json',
+      "x-access-token": getToken(),
+    },
+    timeout: Timeout,
+    data,
+    url: `${REACT_APP_SERVER_URL}/${url}`,
+  };
+  return axios(options);
+};
+
+export const apiFormdata = (url = "", method = "POST", data = {}) => {
+  const options = {
+    method,
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "x-access-token": getToken(),
+    },
+    timeout: Timeout,
+    data,
+    url: `${REACT_APP_SERVER_URL}/${url}`,
+  };
+  return axios(options);
+};
 
 // export const remove = (url = 'delete', method = 'DELETE') => {
 //   const options = {
 //     method,
 //     timeout: Timeout,
-//     url: `${process.env.REACT_APP_BASE_URL_FOR_UPLOAD}${url}`,
+//     url: `${REACT_APP_BASE_URL_FOR_UPLOAD}${url}`,
 //   }
 //   return axios(options)
 // }
 
-const getToken = () => !!cookie.get('user') ? localStorage.getItem('token') : null
+const getToken = () =>
+  !!cookie.get("user") ? localStorage.getItem("token") : null;
 
-export default api
+export default api;
